43edde577d253d38ff31bd90e4b11919de5a3231
juma-vincent <vinsoncode@gmail.com>
2022-02-23 20:12:25 +0300

    modified the footer

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <h4 className="title"> Contact Information</h4>
         <small> Email: njokilesootia@gmail.com </small> <br />
         <small>Phone: +254 701 032469</small> <br />
-        <small>&copy; 2021 Lesootiakdecore . All rights reserved.</small>
+        <small>&copy; { new Date().getFullYear().toString()} Lesootiakdecore . All rights reserved.</small>
       </div>
     </div>
   );
