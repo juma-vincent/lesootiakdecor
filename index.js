8d1e13f5fd42661e672c0016461d178359c3c522
Vincent Juma <61782453+juma-vincent@users.noreply.github.com>
2022-11-19 09:34:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('./models/Product');
 require('./models/Category');
 
 
-mongoose.connect('mongodb+srv://lesootia:tutie001@lesootiakdecor-prod.xpnak.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://lesootia:<tutie001>@lesootiakdecor-prod.xpnak.mongodb.net/?retryWrites=true&w=majority')
 // mongoose.connect(keys.mongoURI)
 console.log(keys);
 app.use(bodyParser.json());
