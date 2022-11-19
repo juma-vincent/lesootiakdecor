2a2f0aad6b3cb10400e924771a9ed29d872b0753
Vincent Juma <61782453+juma-vincent@users.noreply.github.com>
2022-11-19 09:07:04 +0300

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
 //     mongoURI: process.env.MONGO_URI,
-    mongoURI: 'mongodb+srv://lesootia:<tutie001>@lesootiakdecor-prod.xpnak.mongodb.net/?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://lesootia:tutie001@lesootiakdecor-prod.xpnak.mongodb.net/?retryWrites=true&w=majority'
    
 } 
